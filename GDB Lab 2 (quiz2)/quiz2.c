ad7be693a6aec3fb12513a131553e03b0bd7d4bc
nuannim <66070286@kmitl.ac.th>
2024-07-27 15:43:21 +0700

    quiz2

---
+++ GDB Lab 2 (quiz2)/quiz2.c
@@ -0,0 +1,43 @@
+#include "stdio.h"
+
+int main() {
+    int i=5, j=36;
+    float x=0.001, y=0.00, z=-81.753;
+    char c='z', d='M';
+
+
+    // Assignment Statements of each question
+
+    // 1
+    // y-=(c+d)/i+x;
+    
+    // 2
+    // y=x=z-j;
+
+    // 3
+    // i=c%j
+
+    // 4
+    // j*=(int)(x+z)/2
+
+    // 5
+    // (y-x)>=z
+    
+    // 6
+    // c=i+=j
+
+    // 7
+    // (x*j) >= (z*i)
+    
+    // 8
+    // j*(i-7)>(y-2)
+    
+    // 9
+    // z/j
+    
+    // 10
+    (d <= 'a') && ('A' >= c-30)
+    ;
+
+    return 0;
+}
