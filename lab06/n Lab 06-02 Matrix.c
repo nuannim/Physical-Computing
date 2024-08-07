a867fda63e51e07df170694fc3dbe3a007ddbdcf
nuannim <66070286@kmitl.ac.th>
2024-08-08 00:18:39 +0700

    lab06 not finish

---
+++ lab06/n Lab 06-02 Matrix.c
@@ -0,0 +1,30 @@
+// #include <stdio.h>
+
+// int main() {
+//     double matrix1[2][2];
+//     // double matrix2[][];
+
+//     for(int i = 0; i < 3; i++) {
+//         // printf("i = %d", i);
+
+//         for (int j = 0; j < 3; j++) {
+//             // printf("i%dj%d ", i, j);
+//             scanf("%lf", &matrix1[i][j]);
+//         }
+//         printf("\n");
+//     }
+
+//     printf("========check=======\n");
+
+//     for(int i = 0; i < 3; i++) {
+//         // printf("i = %d", i);
+
+//         for (int j = 0; j < 3; j++) {
+//             // printf("i%dj%d ", i, j);
+//             printf("%lf", matrix1[i][j]);
+//         }
+//         printf("\n");
+//     }    
+
+//     return 0;
+// }
